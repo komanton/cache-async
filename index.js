b4855d8b4c97f47720991c8145bdec2500f1570e
anton <anton@anton.com>
2016-03-27 13:10:48 +0300

    test #3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function($timeout) {
     var cacheItems = [];
     var get = function(key, action, timeout) {
         return new Promise(function(resolve, reject) {
+            console.info('get ->');
             if (!timeout) {
                 timeout = 60;
             }
@@ -15,21 +16,25 @@ module.exports = function($timeout) {
                 cacheItems.push(cacheItem);
             }
             if (cacheItem.value.expires != undefined && (now < cacheItem.value.expires)) {
+                console.info('get from cache');
+                console.info('Now: ' + now + ' expires ' + cacheItem.value.expires);
                 resolve({ key: key, value: cacheItem.value.data });
             } else {
                 if (cacheItem.lock === false) {
                     cacheItem.lock = true;
-                    action().then(function(result) {
+                    action().then(function(result) {                        
                         cacheItem.value.data = result;
                         var expires = new Date(now.getTime());
-                        expires.setSeconds(expires.getSeconds() + timeout);
+                        expires.setMilliseconds(expires.getMilliseconds() + timeout);
                         cacheItem.value.expires = expires;
                         //console.info('Now: ' + now + ' expires ' + cacheItem.value.expires);
                         cacheItem.lock = false;
+                        console.info('populate cache and get');
                         resolve({ key: key, value: cacheItem.value.data });
                     }).catch(reject);
                 } else {
                     var wait = function() {
+                        console.info('cache wait');
                         if (cacheItem.lock === false) {
                             resolve({ key: key, value: cacheItem.value.data });
                         } else {
--- test/index.js
+++ test/index.js
@@ -1,19 +1,23 @@
 require('es6-shim');
+
 var chai = require("chai"), 
     chaiAsPromised = require("chai-as-promised"),
     sinonChai = require("sinon-chai"),
     sinon = require("sinon");
-    
+require('sinon-as-promised')
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 var should = chai.should(),    
-    assert = require('assert');
+    assert = require('assert'),
+    tk = require('timekeeper');
     
 var Cache = require('../index');
     
 var stubAsync = function(value, timeout, needReject) {
     return new Promise(function(resolve, reject) {
+        console.log('async start ...');
         setTimeout(function() {
+            console.log('async end!');
             if (needReject == undefined || needReject === false) {
                 resolve(value);
             } else {
@@ -32,28 +36,33 @@ describe('Cache', function() {
         cache = Cache(setTimeout);
     });
     afterEach(function() {
-
+         tk.reset();
     });
     describe('get', function() {
         it('should return key-value when async func resolved', function(done) {
             cache.get(stubKV.key, stubAsyncResolve, 10)
                 .should.eventually.deep.equal(stubKV).notify(done);
         });
-
         it('should return cached key-value when async func resolved', function(done) {
-            var callback = sinon.stub().returns(1);
-            var dd = function(){return stubAsyncResolve().then(function(){
-                return callback();});}
-            cache.get(stubKV.key, dd, 10)
+            var stubCallBack = sinon.stub().resolves(1);            
+            cache.get(stubKV.key, stubCallBack, 500)
                 .should.eventually.deep.equal(stubKV)
-                .then(cache.get(stubKV.key, dd, 10))
-                .should.eventually.deep.equal(stubKV)
-                .should.be.fulfilled
-                .then(function(){callback.should.have.been.calledOnce})              
+                .then(function() {return cache.get(stubKV.key, stubCallBack, 10)})
+                .should.eventually.deep.equal(stubKV)                
+                .then(function(){stubCallBack.should.have.been.calledOnce})              
                 .should.notify(done);
         });
-        it('should ---- when async have not resolved', function() {
-            
+        it('should return non-cached key-value when cache timeout', function(done) {
+            var stubCallBack = sinon.stub().resolves(1);
+            cache.get(stubKV.key, stubCallBack, 500)
+                .should.eventually.deep.equal(stubKV)
+                .then(function() {return cache.get(stubKV.key, stubCallBack, 10)})
+                .should.eventually.deep.equal(stubKV)                
+                .then(function() {return stubAsync(null, 550)})//expire cache                
+                .then(function() {return cache.get(stubKV.key, stubCallBack, 10)})
+                .should.eventually.deep.equal(stubKV)                
+                .then(function(){stubCallBack.should.have.been.calledTwice})              
+                .should.notify(done);
         });
     });
 });
